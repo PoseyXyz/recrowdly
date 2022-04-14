366d1d12d169dc612240a576e5bbfd3dea6b83ca
Posey <adeposi60@gmail.com>
2022-04-14 17:21:34 +0100

    Setup container structure

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../styles/Header.module.scss'
+
+const Header=()=> {
+    return (
+        <nav>
+            <h1>Hello</h1>
+        </nav>
+    );
+}
+
+export default Header;
