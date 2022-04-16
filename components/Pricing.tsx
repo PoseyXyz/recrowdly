7309f16cd49c6ab959ce74a1503eef61b4ae0286
Posey <adeposi60@gmail.com>
2022-04-16 18:06:02 +0100

    Created relevant files for pricicng section

---
+++ components/Pricing.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../styles/Pricing.module.scss'
+
+function Pricing() {
+    return (
+        <section className={styles.pricing}>
+            
+        </section>
+    );
+}
+
+export default Pricing;
