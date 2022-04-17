46f679bbce78edf2dce443446d618a41fbaf84f5
Posey <adeposi60@gmail.com>
2022-04-17 17:16:24 +0100

    Created relevant files for testimonials section

---
+++ components/Testimonials.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from '../styles/Testimonials.module.scss'
+function Testimonials() {
+    return (
+        <section className={styles.testimonials}>
+            
+        </section>
+    );
+}
+
+export default Testimonials;
